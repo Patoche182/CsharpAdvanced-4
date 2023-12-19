d6ce132cdc72326394d1b3dc5a94a4624ad43c6a
PatriceHANNE <patrice.hanne@gmail.com>
2023-12-19 16:03:04 +0100

    Ajoutez des fichiers projet.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace CsharpAdvanced_4
+{
+    public interface IMath
+    {
+        int Add(int a, int b);
+        int Sub(int a, int b);
+        int Mul(int a, int b);
+        int Div(int a, int b);
+    }
+
+    internal class Program : IMath
+    {
+        public static Program p = new Program();
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(p.Add(1,1));
+        }
+
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Div(int a, int b)
+        {
+            return a / b;
+        }
+
+        public int Mul(int a, int b)
+        {
+            return a * b;
+        }
+
+        public int Sub(int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
